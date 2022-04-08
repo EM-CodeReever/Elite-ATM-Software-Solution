2aac11bce4ceb9e5b8b2b79fcd78106a0aea7766
Svengalist <elliotmorrison58@gmail.com>
2022-04-08 12:18:57 -0500

    Added a MianWindow MDI

---
+++ Elite ATM Software/FrmMainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elite_ATM_Software
+{
+    public partial class FrmMainWindow : Form
+    {
+        public FrmMainWindow()
+        {
+            InitializeComponent();
+            FrmLogin frmLogin = new FrmLogin();
+            frmLogin.MdiParent = this;
+            frmLogin.Show();
+        }
+    }
+}
