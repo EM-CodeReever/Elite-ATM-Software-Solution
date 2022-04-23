2bceecab4abc14472cf171c533e46b37e96641c5
Svengalist <elliotmorrison58@gmail.com>
2022-04-23 09:38:35 -0500

    Added static class to track currently logged in user

---
+++ Elite ATM Software/UserTracker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elite_ATM_Class_Library;
+
+namespace Elite_ATM_Software
+{
+    public static class UserTracker
+    {
+        public static User currentUser;
+    }
+}
